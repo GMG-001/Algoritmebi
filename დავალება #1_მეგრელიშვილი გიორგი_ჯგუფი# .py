babdaab8164f0f3bbb3b3e132c95a2bf7d7ed4f7
giorgi megrelishvili <giorgi@localhost.localdomain>
2020-09-21 11:23:45 +0400

    progres_regres

---
+++ დავალება #1_მეგრელიშვილი გიორგი_ჯგუფი# .py
@@ -0,0 +1,28 @@
+# #3_1
+# (a,b,c,d,e)=(20,5,300,500,510)
+
+# if (a>b) and (a>c) and (a>d) and (a>e):
+#     largest=a
+# elif (a<b) and (b>c) and (b>d) and (b>e):
+#     largest=b
+# elif (c>a) and (c>b) and (c>d) and (c>e):
+#     largest=c
+# elif (d>a) and (d>b) and (c<d) and (d>e):
+#     largest=d
+# else:
+#     largest=e
+# print(largest)
+
+#3_2
+(a,b,c,d,e)=(10,5,3,50,15)
+if a>b:
+    if a>c:
+        if a>d:
+            if a>e:
+                print(a)
+elif b>a:
+    if b>c:
+        if b>d:
+            print(b)
+else:
+    print(e)
