60ed1706559e885333cc58972f11f9964eac68f6
giorgi megrelishvili <giorgi@localhost.localdomain>
2020-09-21 11:42:33 +0400

    algo_testing

--- დავალება #1_მეგრელიშვილი გიორგი_ჯგუფი# .py
+++ დავალება #1_მეგრელიშვილი გიორგი_ჯგუფი# .py
@@ -13,27 +13,27 @@
 #     largest=e
 # print(largest)
 
-#3_2
-(a,b,c,d,e)=(10,5,3,50,15)
-if a>b:
-    if a>c:
-        if a>d:
-            if a>e:
-                print(a)
-elif b>a:
-    if b>c:
-        if b>d:
-            if b>e:
-                print(b)
-elif c>a:
-    if c>b:
-        if b>d:
-            if c>e:
-                print(c)
-elif d>a:
-    if d>b:
-        if d>c:
-            if d>e:
-                print(d)
-else:
-    print(e)
+# #3_2
+# (a,b,c,d,e)=(10,5,3,50,15)
+# if a>b:
+#     if a>c:
+#         if a>d:
+#             if a>e:
+#                 print(a)
+# elif b>a:
+#     if b>c:
+#         if b>d:
+#             if b>e:
+#                 print(b)
+# elif c>a:
+#     if c>b:
+#         if b>d:
+#             if c>e:
+#                 print(c)
+# elif d>a:
+#     if d>b:
+#         if d>c:
+#             if d>e:
+#                 print(d)
+# else:
+#     print(e)
