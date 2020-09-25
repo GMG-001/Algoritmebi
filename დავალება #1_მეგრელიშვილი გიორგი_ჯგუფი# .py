c884aeca0b7005ef6b8f9536d7936cc7eb7d70bb
giorgi megrelishvili <giorgi@localhost.localdomain>
2020-09-25 18:52:52 +0400

    progres

--- დავალება #1_მეგრელიშვილი გიორგი_ჯგუფი# .py
+++ დავალება #1_მეგრელიშვილი გიორგი_ჯგუფი# .py
@@ -13,27 +13,36 @@
 #     largest=e
 # print(largest)
 
-# #3_2
-# (a,b,c,d,e)=(105,55,35,50,155)
-# if a>b:
-#     if a>c:
-#         if a>d:
-#             if a>e:
-#                 print(a)
-#             else:
-#                 print(e)
-#         else:
-#             if d>e:
-#                 print(d)
-#             else:
-#                 print(e)
-#     else:
-#         if c>d:
-#             print(c)
-#         else:
-#             print(d)
-# else:
-#     if b>c:
-#         print(b)
-#     else:
-#         print(c)
+#3_2
+(a,b,c,d,e)=(60,55,105,50,155)
+if a>b:
+    if a>c:
+        if a>d:
+            if a>e:
+                print(a)
+            else:
+                print(e)
+        else:
+            if d>e:
+                print(d)
+            else:
+                print(e)
+    else:
+        if c>d:
+            if c>e:
+                print(c)
+        else:
+            print(e)
+else:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+
+# # 2 
+# a=[]
+# g=int(input('ricxvi '))
+# for i in range(g):
+#     b=i%2
+#     a.append(b)
+#     print(a)
